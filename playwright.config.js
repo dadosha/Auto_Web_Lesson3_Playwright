54e9771bcdb99a5a93d4adc2a0a42b4611149ee6
dadosha <55433356+dadosha@users.noreply.github.com>
2024-09-09 17:39:45 +0300

    Add Screenshots

---
+++ playwright.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    use: {
+        screenshot: 'only-on-failure', // Скриншоты будут сделаны только при сбоях
+    },
+};
--- tests/App.spec.js
+++ tests/App.spec.js
@@ -2,6 +2,7 @@ const { test, expect, chromium} = require("@playwright/test");
 const { email, password } = require("../user");
 
 test('Login in netology', async ({ page }) => {
+
   await page.goto('https://netology.ru/');
 
   await page.click('text=Войти');
@@ -11,6 +12,7 @@ test('Login in netology', async ({ page }) => {
   await page.click('[data-testid="login-submit-btn"]');
 
   await page.waitForSelector('[data-testid="profile-programs-content"] h2');
+  await page.screenshot({ path: './screenshots/Login.png', fullPage: true });
   await expect(page.locator('[data-testid="profile-programs-content"] h2')).toHaveText('Моё обучение')
 });
 
@@ -23,9 +25,8 @@ test('Incorrect login in netology', async ({ page }) => {
   await page.fill('[placeholder="Email"]', 'test@test.ru');
   await page.fill('[placeholder="Пароль"]', '12345qwert');
 
-
   await page.click('[data-testid="login-submit-btn"]');
-
+  await page.screenshot({ path: './screenshots/incorrectLogin.png', fullPage: true });
   await expect(page.locator('[data-testid="login-error-hint"]'))
       .toHaveText(/(Вы ввели неправильно логин или пароль.|Слишком много попыток, вход временно заблокирован. Попробуйте авторизоваться через 30 секунд.)/);
 });
